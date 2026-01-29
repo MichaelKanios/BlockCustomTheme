a4f0e8825b2782b288257dab1666d08f61d818bb
Michael Kanios <mkanios77@gmail.com>
2026-01-29 11:09:29 +0000

    theme

--- MyBlockTheme/functions.php
+++ MyBlockTheme/functions.php
@@ -4,4 +4,9 @@ add_action( 'after_setup_theme', function() {
 	remove_theme_support( 'core-block-patterns' );
 } );
 
+//Enqueue styles
+function myblocktheme_enqueue_styles() {
+	wp_enqueue_style( 'myblocktheme-style', get_stylesheet_uri(),array(),wp_get_theme()->get( 'Version' ) );
+}	
+add_action( 'wp_enqueue_scripts', 'myblocktheme_enqueue_styles' );
 ?>
